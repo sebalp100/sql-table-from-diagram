c2e8c9a0829892cf98b747d9bcfbc52cf96a96bf
Sebastian Martin <sebalp100@gmail.com>
2023-02-01 13:56:35 -0300

    Add patients, medical_histories, invoices tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,1 +1,22 @@
+CREATE TABLE patients (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    date_of_birth DATE
+);
 
+CREATE TABLE medical_histories (
+    id SERIAL PRIMARY KEY,
+    admited_at TIMESTAMP,
+    patient_id INTEGER REFERENCES patients(id) ON DELETE CASCADE,
+    status VARCHAR(100),
+    UNIQUE (patient_id)
+);
+
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INTEGER REFERENCES medical_histories(id) ON DELETE CASCADE,
+    UNIQUE (medical_history_id)
+);
